d261ab8a6857d0ff742108ed6d2a52e207fce132
erj32797-an <erj32797@gmail.com>
2026-09-20 20:20:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/goods-map-aiapp/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
